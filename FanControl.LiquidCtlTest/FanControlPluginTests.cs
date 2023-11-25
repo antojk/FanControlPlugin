44050dbf0a530fc46bcb1ea966485a83bcbeb228
Antony John <antojk@gmail.com>
2023-11-26 00:44:51 +0530

    Added test cases and fixed bugs 1. Config Manager is now working correctly with safeguards for missing config file etc.. 2. Plugin init is fully refined to enumerate all devices and filter out misbehaving ones

--- FanControl.LiquidCtlTest/FanControlPluginTests.cs
+++ FanControl.LiquidCtlTest/FanControlPluginTests.cs
@@ -9,29 +9,31 @@ public class FanControlPluginTests
     {
     }
 
+
     [Test]
     public void TestIfPluginLoadedCorrectly()
     {
         var pluginLogger = new Moq.Mock<IPluginLogger>();
-        pluginLogger.Setup(x => x.Log(It.IsAny<string>()));
+        pluginLogger.Setup(x => x.Log(It.IsAny<string>())).Verifiable();
         var dialog = new Mock<IPluginDialog>();
-        var plugin = new LiquidCtlPlugin(pluginLogger.Object,dialog.Object);
-        Assert.That(plugin, Is.Not.Null); 
+        var plugin = new LiquidCtlPlugin(pluginLogger.Object, dialog.Object);
+        Assert.That(plugin, Is.Not.Null);
         pluginLogger.Verify(logger => logger.Log(It.IsAny<String>()), Times.AtLeast(1));
-        
     }
 
-    [Test] public void TestIfPluginInitializingCorrectly()
+    [Test]
+    public void TestIfPluginInitializingCorrectly()
     {
         var pluginLogger = new Moq.Mock<IPluginLogger>();
-        pluginLogger.Setup(x => x.Log(It.IsAny<string>()));
+        pluginLogger.Setup(x => x.Log(It.IsAny<string>()))
+            .Callback((string msg) => Console.WriteLine($"Log received: {msg}"))
+            .Verifiable();
         var dialog = new Mock<IPluginDialog>();
-        var task = new Mock<Task>();
-        dialog.Setup(x => x.ShowMessageDialog(It.IsAny<string>())).Returns(() => task.Object);
-        var plugin = new LiquidCtlPlugin(pluginLogger.Object,dialog.Object);
+
+        dialog.Setup(x => x.ShowMessageDialog(It.IsAny<string>())).Returns(() => Task.CompletedTask);
+        var plugin = new LiquidCtlPlugin(pluginLogger.Object, dialog.Object);
         plugin.Initialize();
+        pluginLogger.Verify(logger => logger.Log(It.IsAny<string>()), Times.AtLeastOnce);
         Assert.That(plugin.IsInited());
-        pluginLogger.Verify(logger => logger.Log(It.IsAny<String>()), Times.Once);
-        
     }
 }
