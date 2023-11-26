94ad1aeef25f89e0f872fcc8f878ed9818321288
Antony John <antojk@gmail.com>
2023-11-26 22:10:12 +0530

    Added Tests for Device class

--- FanControl.LiquidCtlTest/LiquidctlDeviceTests.cs
+++ FanControl.LiquidCtlTest/LiquidctlDeviceTests.cs
@@ -27,5 +27,33 @@ public class LiquidctlDeviceTests
             Assert.That(device is { FanControlSensors.Count: 3 });
         });
     }
+
+    [Test]
+    public void TestCreateLiquidCtlDeviceWithPumpData()
+    {
+        var pluginLogger = new Mock<IPluginLogger>();
+        pluginLogger.Setup(x => x.Log(It.IsAny<string>()))
+            .Callback((string msg) => Console.WriteLine($"{msg}"))
+            .Verifiable();
+        var statusJson =
+            JsonConvert.DeserializeObject<LiquidctlStatusJSON>(File.ReadAllText("tests_pump_temp_status.json"));
+        var device = new LiquidctlDevice(statusJson, pluginLogger.Object);
+        Assert.Multiple(() =>
+        {
+            Assert.That(device is not null);
+            Assert.That(device is { HasFanSpeed: false });
+            Assert.That(device is { HasPumpDuty: true });
+            Assert.That(device is { HasLiquidTemperature: true });
+            Assert.That(device is { LiquidTemperatureSensor: not null });
+            Assert.That(device is { PumpSpeedSensor: not null });
+            Assert.That(device?.PumpSpeedSensor?.Name is "Pump - NZXT Kraken Pump",$"{device?.PumpSpeedSensor?.Name}");
+            Assert.That(device is { PumpDutyController: not null });
+            Assert.That(device?.PumpDutyController?.Name is "Pump Control - NZXT Kraken Pump",$"{device?.PumpDutyController?.Name}");
+            Assert.That(device is { LiquidTemperatureSensor: not null });
+            Assert.That(device?.LiquidTemperatureSensor?.Name is "Liquid Temp. - NZXT Kraken Pump",$"{device?.LiquidTemperatureSensor?.Name}");
+            Assert.That(device is { FanSpeedSensors.Count: 0});
+            Assert.That(device is { FanControlSensors.Count: 0 });
+        });
+    }
     
 }
