fbc5ee6805be7cdeae781d50b4157d8f03313a3b
Antony John <antojk@gmail.com>
2023-12-01 13:59:00 +0530

    1. Added additional tests to cover plugin load and status retrieval 2. Refactored test setup and teardonw

--- FanControl.LiquidCtlTest/FanControlPluginTests.cs
+++ FanControl.LiquidCtlTest/FanControlPluginTests.cs
@@ -4,6 +4,47 @@ namespace FanControl.LiquidCtlTest;
 
 public class FanControlPluginTests
 {
+    private static void RecursiveCopy(string sourceDirectory, string targetDirectory)
+    {
+        // Create the target directory if it doesn't exist
+        Directory.CreateDirectory(targetDirectory);
+
+        // Get all files in the source directory and copy them to the target directory
+        foreach (var file in Directory.GetFiles(sourceDirectory))
+        {
+            var targetFile = Path.Combine(targetDirectory, Path.GetFileName(file));
+            File.Copy(file, targetFile, true);
+        }
+
+        // Recursively copy subdirectories
+        foreach (var directory in Directory.GetDirectories(sourceDirectory))
+        {
+            var targetSubDirectory = Path.Combine(targetDirectory, Path.GetFileName(directory));
+            RecursiveCopy(directory, targetSubDirectory);
+        }
+    }
+
+    private static void RecursiveDelete(string targetDirectory)
+    {
+        if (!Directory.Exists(targetDirectory)) return;
+
+        // Delete files within the directory
+        foreach (var file in Directory.GetFiles(targetDirectory))
+        {
+            File.Delete(file);
+        }
+
+        // Recursive delete for subdirectories
+        foreach (var subDirectory in Directory.GetDirectories(targetDirectory))
+        {
+            RecursiveDelete(subDirectory);
+        }
+
+        // Delete the directory itself
+        Directory.Delete(targetDirectory);
+    }
+
+
     [SetUp]
     public void Setup()
     {
@@ -18,9 +59,20 @@ public class FanControlPluginTests
                     return df.Exists;
                 });
 
-            var liquidCtlSource = Path.Combine(driveLetter, "Fan Control", "Plugins", "liquidctl.exe");
-            if (File.Exists(liquidCtlSource))
-                File.Copy(liquidCtlSource, Path.Combine(Environment.CurrentDirectory, "liquidctl.exe"), false);
+            var fanCtrlPluginSrc = Path.Combine(driveLetter, "Fan Control", "Plugins");
+            var liquidCtlSrc = Path.Combine(fanCtrlPluginSrc, "liquidCtl");
+            if (!Directory.Exists(liquidCtlSrc)) return;
+            var sourceDir = Directory.CreateDirectory(fanCtrlPluginSrc);
+            if (Directory.Exists(Path.Combine(Environment.CurrentDirectory, "liquidCtl")))
+            {
+                var destinationDir = Environment.CurrentDirectory;
+                if (File.Exists(Path.Combine(destinationDir, "liquidCtl", "liquidCtl.exe"))) return;
+            }
+            else
+            {
+                var targetDir = Path.Combine(Environment.CurrentDirectory, "liquidCtl");
+                RecursiveCopy(liquidCtlSrc, targetDir);
+            }
         }
         catch (InvalidOperationException e)
         {
@@ -31,8 +83,7 @@ public class FanControlPluginTests
     [TearDown]
     public void TearDown()
     {
-        if (File.Exists(Path.Combine(Environment.CurrentDirectory, "liquidctl.exe")))
-            File.Delete(Path.Combine(Environment.CurrentDirectory, "liquidctl.exe"));
+        RecursiveDelete(Path.Combine(Environment.CurrentDirectory, "liquidCtl"));
         if (File.Exists(Path.Combine(Environment.CurrentDirectory, "config.yaml")))
             File.Delete(Path.Combine(Environment.CurrentDirectory, "config.yaml"));
     }
@@ -98,6 +149,96 @@ public class FanControlPluginTests
             Assert.That(pluginSensors, Has.Count.EqualTo(pluginControlSensors.Count));
             Assert.That(pluginSensors.First() is not null);
             Assert.That(pluginControlSensors.First() is not null);
+            Assert.That(pluginSensors.First().Name, Is.EqualTo("Fan 1 - NZXT RGB & Fan Controller (3+6 channels)"));
+            Assert.That(pluginSensors.First().Id,
+                Is.EqualTo(
+                    "\\\\?\\HID#VID_1E71&PID_2019#8&1364b162&0&0000#{4d1e55b2-f16f-11cf-88cb-001111000030}-fanRPM1"));
+            Assert.That(pluginControlSensors.First().Name,
+                Is.EqualTo("Fan 1 Control - NZXT RGB & Fan Controller (3+6 channels)"));
+            Assert.That(pluginControlSensors.First().Id,
+                Is.EqualTo(
+                    "\\\\?\\HID#VID_1E71&PID_2019#8&1364b162&0&0000#{4d1e55b2-f16f-11cf-88cb-001111000030}-fanCtrl1"));
         });
+    }
+
+    [Test]
+    public void TestIfPluginGettingSensorData()
+    {
+        var pluginLogger = new Mock<IPluginLogger>();
+        pluginLogger.Setup(x => x.Log(It.IsAny<string>()))
+            .Callback((string msg) => Console.WriteLine($"Log received: {msg}"))
+            .Verifiable();
+        var dialog = new Mock<IPluginDialog>();
+        dialog.Setup(x => x.ShowMessageDialog(It.IsAny<string>())).Returns(() => Task.CompletedTask);
+
+        var mockSensorContainer = new Mock<IPluginSensorsContainer>();
+        var pluginSensors = new List<IPluginSensor>();
+        mockSensorContainer.Setup(x => x.FanSensors).Returns(pluginSensors);
+
+        var pluginControlSensors = new List<IPluginControlSensor>();
+        mockSensorContainer.Setup(x => x.ControlSensors).Returns(pluginControlSensors);
+        var plugin = new LiquidCtlPlugin(pluginLogger.Object, dialog.Object);
+        plugin.Initialize();
+
+        // Load sequence
+        plugin.Load(mockSensorContainer.Object);
+        mockSensorContainer.Verify(x => x.FanSensors, Times.Once);
+        mockSensorContainer.Verify(x => x.ControlSensors, Times.Once);
+
+        // Update the sensor statuses
+        plugin.Update();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(pluginSensors.First() is not null);
+            Assert.That(pluginControlSensors.First() is not null);
+            pluginSensors.ToList().ForEach((IPluginSensor sensor) =>
+            {
+                Assert.That(sensor.Value, Is.GreaterThan(500)); // 500 is the bottom speed of the fans in question
+            });
+            pluginControlSensors.ToList().ForEach((IPluginControlSensor ctrlSensor) =>
+            {
+                Assert.That(ctrlSensor.Value, Is.EqualTo(25)); // 25% is the default duty cycle of the NZXT controller
+            });
+
+            Assert.That(pluginSensors.First().Value, Is.Not.EqualTo(pluginSensors.Last().Value));
+        });
+    }
+
+
+    [Test]
+    public async Task TestIfPluginSettingFanDuty()
+    {
+        var pluginLogger = new Mock<IPluginLogger>();
+        pluginLogger.Setup(x => x.Log(It.IsAny<string>()))
+            .Callback((string msg) => Console.WriteLine($"Log received: {msg}"))
+            .Verifiable();
+        var dialog = new Mock<IPluginDialog>();
+        dialog.Setup(x => x.ShowMessageDialog(It.IsAny<string>())).Returns(() => Task.CompletedTask);
+
+        var mockSensorContainer = new Mock<IPluginSensorsContainer>();
+        var pluginSensors = new List<IPluginSensor>();
+        mockSensorContainer.Setup(x => x.FanSensors).Returns(pluginSensors);
+
+        var pluginControlSensors = new List<IPluginControlSensor>();
+        mockSensorContainer.Setup(x => x.ControlSensors).Returns(pluginControlSensors);
+        var plugin = new LiquidCtlPlugin(pluginLogger.Object, dialog.Object);
+        plugin.Initialize();
+
+        // Load sequence
+        plugin.Load(mockSensorContainer.Object);
+        mockSensorContainer.Verify(x => x.FanSensors, Times.Once);
+        mockSensorContainer.Verify(x => x.ControlSensors, Times.Once);
+
+        Assert.That(pluginControlSensors.First(), Is.InstanceOf(typeof(IPluginControlSensor)));
+        ((IPluginControlSensor)pluginControlSensors.First()).Set(100);
+        await Task.Delay(1000);
+        plugin.Update();
+        Assert.Multiple(() =>
+        {
+            Assert.That(pluginControlSensors.First().Value, Is.EqualTo(100));
+            Assert.That(pluginSensors.First().Value, Is.GreaterThanOrEqualTo(2000)); //The rated max speed of the fans are around 2000
+        });
+        ((IPluginControlSensor)pluginControlSensors.First()).Set(25); //Setting back to default
     }
 }
